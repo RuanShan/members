95afd4ea55946d6ae35c227dc93536bf9a605339
david <areq22@aliyun.com>
2015-10-05 12:00:34 +0800

    fix timezone

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module DummyApp
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # Run " rake time:zones:all" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Beijing'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
